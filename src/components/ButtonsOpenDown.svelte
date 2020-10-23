90739a82934ac472d13aec669cebaa4e4a2d8a3b
mamo55 <m.matur@hotmail.fr>
2020-10-23 20:42:42 +0200

    fixed #1 + change accueil architecture

---
+++ src/components/ButtonsOpenDown.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+    import Buttons from './Buttons.svelte'
+
+    export let title 
+    export let cat 
+
+    function handleOpen(){
+        dispatch("buttonOpenDown")
+        console.log('HANDLED ONOFF')
+    }
+</script>
+
+<Buttons title={title} on:click={handleOpen} cat={cat}>
+    <slot/> 
+</Buttons>
